3b3c0df8ee276a4a664dc78e45ddb9aec41b84c5
Pranav rathi <rathipranav07@gmail.com>
2025-07-11 05:19:42 +0000

    Notifications & Alerts too

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -1,3 +1,4 @@
+
 "use client"
 
 import * as React from "react"
@@ -16,7 +17,7 @@ const ToastViewport = React.forwardRef<
   <ToastPrimitives.Viewport
     ref={ref}
     className={cn(
-      "fixed top-0 z-[100] flex max-h-screen w-full flex-col-reverse p-4 sm:bottom-0 sm:right-0 sm:top-auto sm:flex-col md:max-w-[420px]",
+      "fixed top-0 z-[100] flex max-h-screen w-full flex-col-reverse p-4 sm:right-0 sm:top-0 sm:flex-col md:max-w-[420px]",
       className
     )}
     {...props}
@@ -29,9 +30,9 @@ const toastVariants = cva(
   {
     variants: {
       variant: {
-        default: "border bg-background text-foreground",
+        default: "border bg-card/70 text-foreground dark:bg-card/50 dark:backdrop-blur-lg",
         destructive:
-          "destructive group border-destructive bg-destructive text-destructive-foreground",
+          "destructive group border-destructive bg-destructive/70 text-destructive-foreground dark:bg-destructive/50 dark:backdrop-blur-lg [&>button>svg]:text-white",
       },
     },
     defaultVariants: {
