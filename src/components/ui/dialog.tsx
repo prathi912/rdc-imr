bd3042f81fc0e7d392bb43d4dd59b5c8b52f587b
Pranav rathi <rathipranav07@gmail.com>
2025-07-11 06:15:10 +0000

    IT SHOULD BE IN TOP CENTR AND SMALLER, WITH WIDTH LIKE 10 VW OR SMALLER

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -39,7 +39,7 @@ const DialogContent = React.forwardRef<
     <DialogPrimitive.Content
       ref={ref}
       className={cn(
-        "fixed left-[50%] top-[4rem] z-50 grid w-full max-w-md translate-x-[-50%] gap-4 border border-white/10 bg-black/20 p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[4rem] sm:rounded-xl",
+        "fixed left-[50%] top-[4rem] z-50 grid w-full max-w-sm translate-x-[-50%] gap-4 border border-white/10 bg-black/20 p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[4rem] sm:rounded-xl",
         "backdrop-blur-xl",
         className
       )}
