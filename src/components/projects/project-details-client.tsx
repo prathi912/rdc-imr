9680407165d07b336372fb8b60fd62b7da658ff0
Pranav rathi <rathipranav07@gmail.com>
2025-07-19 07:47:44 +0000

    Console Error
    Error: Cannot access 'doc' before initialization
    
    src/com

--- src/components/projects/project-details-client.tsx
+++ src/components/projects/project-details-client.tsx
@@ -238,7 +238,7 @@ export function ProjectDetailsClient({ project: initialProject, allUsers, piUser
         const usersRef = collection(db, "users")
         const q = query(usersRef, where("__name__", "in", project.coPiUids))
         const querySnapshot = await getDocs(q)
-        const fetchedUsers = querySnapshot.docs.map((d) => ({ uid: d.id, ...d.data() }) as User)
+        const fetchedUsers = querySnapshot.docs.map((userDoc) => ({ uid: userDoc.id, ...userDoc.data() }) as User)
         setCoPiUsers(fetchedUsers)
         setCoPiList(fetchedUsers.map((u) => ({ uid: u.uid, name: u.name })))
       }
