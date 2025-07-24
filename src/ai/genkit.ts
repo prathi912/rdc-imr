e51541f5e1c30ec8e866058406c90ca0a80f294c
Pranav rathi <rathipranav07@gmail.com>
2025-07-24 07:25:43 +0000

    API key is present in env file as GENERATIVE_AI_API_KEY

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -2,6 +2,6 @@ import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins: [googleAI({apiKey: process.env.GENERATIVE_AI_API_KEY})],
   model: 'googleai/gemini-2.0-flash',
 });
