2f986da0daf730b93c9153cee93675c5a67274e9
Pranav rathi <rathipranav07@gmail.com>
2025-07-24 07:15:37 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -39,17 +39,21 @@ export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
   asChild?: boolean
+  leftIcon?: React.ReactNode;
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
+  ({ className, variant, size, asChild = false, leftIcon, children, ...props }, ref) => {
     const Comp = asChild ? Slot : "button"
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
         {...props}
-      />
+      >
+        {leftIcon}
+        {children}
+      </Comp>
     )
   }
 )
