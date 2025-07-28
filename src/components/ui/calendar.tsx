14633910976437a8be0e885bd51bb6db42a9dcfc
Pranav rathi <rathipranav07@gmail.com>
2025-07-28 12:05:57 +0000

    see how calendars are behaving

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -7,7 +7,6 @@ import { DayPicker } from "react-day-picker"
 
 import { cn } from "@/lib/utils"
 import { buttonVariants } from "@/components/ui/button"
-import { useIsMobile } from "@/hooks/use-mobile"
 
 export type CalendarProps = React.ComponentProps<typeof DayPicker>
 
@@ -17,12 +16,11 @@ function Calendar({
   showOutsideDays = true,
   ...props
 }: CalendarProps) {
-  const isMobile = useIsMobile();
   return (
     <DayPicker
       showOutsideDays={showOutsideDays}
       className={cn("p-3", className)}
-      numberOfMonths={isMobile ? 1 : 2}
+      numberOfMonths={1}
       classNames={{
         months: "flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0",
         month: "space-y-4",
@@ -50,7 +48,7 @@ function Calendar({
           "bg-primary text-primary-foreground hover:bg-primary hover:text-primary-foreground focus:bg-primary focus:text-primary-foreground",
         day_today: "bg-accent text-accent-foreground",
         day_outside:
-          "day-outside text-muted-foreground aria-selected:bg-accent/50 aria-selected:text-muted-foreground",
+          "day-outside text-muted-foreground opacity-50 aria-selected:bg-accent/50 aria-selected:text-muted-foreground",
         day_disabled: "text-muted-foreground opacity-50",
         day_range_middle:
           "aria-selected:bg-accent aria-selected:text-accent-foreground",
