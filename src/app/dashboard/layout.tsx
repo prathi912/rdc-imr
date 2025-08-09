d48c0231114d0e022378b39f700ca059794824b4
Pranav rathi <rathipranav07@gmail.com>
2025-08-09 09:25:33 +0000

    Reduce or remove margin or padding for all containers on mobiles, it is

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -422,7 +422,7 @@ export default function DashboardLayout({ children }: { children: React.ReactNod
             <UserNav user={user} onLogout={attemptLogout} />
           </div>
         </header>
-        <main className="flex-1 overflow-y-auto p-4 sm:p-6">
+        <main className="flex-1 overflow-y-auto sm:p-6">
           {children}
         </main>
       </SidebarInset>
