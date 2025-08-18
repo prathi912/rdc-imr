9f829d0deb0d81c088395be40996df81b7c368eb
Pranav rathi <rathipranav07@gmail.com>
2025-08-18 11:41:24 +0000

    These pages are not been found

--- src/app/dashboard/emr-evaluations/page.tsx
+++
@@ -1,5 +0,0 @@
-// This file is no longer used and can be deleted.
-// The functionality has been merged into /dashboard/evaluator-dashboard/page.tsx
-export default function DeprecatedEmrEvaluationsPage() {
-  return null;
-}
--- src/app/dashboard/emr-logs/page.tsx
+++
@@ -1,5 +0,0 @@
-// This file is no longer used and can be deleted.
-// The functionality has been merged into /dashboard/emr-management/page.tsx
-export default function DeprecatedEmrLogsPage() {
-  return null;
-}
