71a32095493cfdd1f05c5587daa086d8871cbe71
Pranav rathi <rathipranav07@gmail.com>
2025-08-22 04:46:46 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/incentives/claim-details-dialog.tsx
+++ src/components/incentives/claim-details-dialog.tsx
@@ -1,6 +1,7 @@
 
 'use client';
 
+import * as React from 'react';
 import { useState } from 'react';
 import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogDescription, DialogFooter } from "@/components/ui/dialog";
 import { Button } from '@/components/ui/button';
