766759c3499c1595a69745adb0f1f8bb80d4e911
Pranav rathi <rathipranav07@gmail.com>
2025-12-29 12:18:51 +0000

    DO A DEEP RESEARCH AND FIX THIS

--- next.config.ts
+++ next.config.ts
@@ -32,6 +32,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
   experimental: {
