f697e039850e49c07899671cd474e161ecac8592
Pranav rathi <rathipranav07@gmail.com>
2026-01-05 04:28:11 +0000

    /dashboard/project/BAlke9jy2YgZn1xzdIEf
    Page not found error

--- src/app/dashboard/manage-incentive-claims-admin/page.tsx
+++
@@ -1,5 +0,0 @@
-// This page's content has been moved to /dashboard/manage-incentive-claims.
-// This file is now redundant and can be deleted.
-export default function DeprecatedManageIncentiveClaimsAdminPage() {
-  return null;
-}
