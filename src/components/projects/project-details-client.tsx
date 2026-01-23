1969872996e6787e5c9fb5a14724fa5af62ff665
Pranav rathi <rathipranav07@gmail.com>
2026-01-23 09:51:32 +0000

    Console ReferenceError
    uploadFileToServer is not defined
    src/component

--- src/components/projects/project-details-client.tsx
+++ src/components/projects/project-details-client.tsx
@@ -1,5 +1,4 @@
 
-
 "use client"
 
 import * as React from "react"
@@ -29,6 +28,7 @@ import {
   getSystemSettings,
   generateSanctionOrder,
   adminUploadProposal,
+  uploadFileToServer,
 } from "@/app/actions"
 import { generateRecommendationForm } from "@/app/document-actions"
 import { findUserByMisId } from '@/app/userfinding';
