8c06248faf36d3c88a0b309ff5523f35d627c1e1
Pranav Rathi <87274086+prathi912@users.noreply.github.com>
2026-04-04 16:46:38 +0530

    Update config.ts

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -11,18 +11,18 @@ const projectId = process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID;
 let authDomain = process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN;
 
 if (!authDomain && projectId) {
-  authDomain = `${projectId}.firebaseapp.com`;
+    authDomain = `${projectId}.firebaseapp.com`;
 }
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API,
-  authDomain: authDomain,
-  projectId: projectId,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: authDomain,
+    projectId: projectId,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
 };
 
 export const isFirebaseInitialized = !!(
